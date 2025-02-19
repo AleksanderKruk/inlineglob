b80f602e107fa514858b5d832009b9529d1e0b1a
Aleksander Kruk <aleksanderjakubkruk@gmail.com>
2025-02-19 18:57:38 +0100

    added local variable as valid location

--- src/main/java/inline/glob/InlineGlob.java
+++ src/main/java/inline/glob/InlineGlob.java
@@ -3,7 +3,7 @@ package inline.glob;
 import java.lang.annotation.*;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.FIELD)
+@Target({ElementType.FIELD, ElementType.LOCAL_VARIABLE})
 public @interface InlineGlob {
     public String glob() default "";
 }
