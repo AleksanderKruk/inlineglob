ed3cf38282623d2c125c7e3f01df789fb73b642c
Aleksander Kruk <aleksanderjakubkruk@gmail.com>
2025-02-26 04:53:05 +0100

    added fixed error with eos match

--- src/test/java/inline/glob/GlobInlinerTest.java
+++ src/test/java/inline/glob/GlobInlinerTest.java
@@ -21,4 +21,34 @@ public class GlobInlinerTest
         assertFalse(predicate.test("abcde"));
         assertFalse(predicate.test("abd"));
     }
+
+    @Test
+    public void anyChar()
+    {
+        var predicate = GlobInliner.compile("?");
+        assertTrue(predicate.test("a"));
+        assertTrue(predicate.test("*"));
+        assertTrue(predicate.test("?"));
+        assertFalse(predicate.test(""));
+    }
+
+    // @Test
+    // public void simplePattern()
+    // {
+    //     var predicate = GlobInliner.compile("abcd");
+    //     assertTrue(predicate.test("abcd"));
+    //     assertFalse(predicate.test("bcd"));
+    //     assertFalse(predicate.test("abcde"));
+    //     assertFalse(predicate.test("abd"));
+    // }
+
+    // @Test
+    // public void simplePattern()
+    // {
+    //     var predicate = GlobInliner.compile("abcd");
+    //     assertTrue(predicate.test("abcd"));
+    //     assertFalse(predicate.test("bcd"));
+    //     assertFalse(predicate.test("abcde"));
+    //     assertFalse(predicate.test("abd"));
+    // }
 }
